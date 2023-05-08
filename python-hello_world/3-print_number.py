6e21c69885b9d284ebda362892913d45308777c1
Nomad-Rob <rfarley89@gmail.com>
2023-05-08 09:52:45 -0700

    another test

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# #Complete this source code in order to print the integer stored in the variable number, 
-# #followed by Battery street, followed by a new line.
+# #Complete this source code in order to print the integer stored in the
+# #variable followed by Battery street, followed by a new line.
 number = 98
 print(f"{number} Battery street")
