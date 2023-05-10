34cb37d975a47f08e3f509161e8ade172b409fb4
Nomad-Rob <rfarley89@gmail.com>
2023-05-10 07:20:27 -0700

    fixed pep8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,10 @@ def uppercase(string):
     output = ''
     for char in string:
         ascii_val = ord(char)
-        if 97 <= ascii_val <= 122:  # Check if lowercase character
-            uppercase_char = chr(ascii_val - 32)  # Convert to uppercase ASCII value
+        # Check if lowercase character
+        if 97 <= ascii_val <= 122:
+            # Convert to uppercase ASCII value
+            uppercase_char = chr(ascii_val - 32)
             output += uppercase_char
         else:
             output += char
