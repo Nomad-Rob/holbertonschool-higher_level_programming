8b77de7b562aa14cb68bf8438a4e626c8ed41eb5
Nomad-Rob <rfarley89@gmail.com>
2023-05-10 07:27:29 -0700

    changed the code after running the checker

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = abs(number) % 10
-    print("Last digit: {}".format(last_digit))
-    return last_digit
+    result = abs(number) % 10
+    print(result, end="")
+    return result
