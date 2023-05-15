d612e13ef1f8a441f10ecb1c3ab4afa552ef43b9
Rob <115587964+Nomad-Rob@users.noreply.github.com>
2023-05-15 13:40:07 -0500

    adding files before pep8 check

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Write a function that returns a new dictionary with all values multiplied by 2
+def multiply_by_2(a_dictionary):
+    new_dict = {}
+    for key, value in a_dictionary.items():
+        new_dict[key] = value * 2
+    return new_dict
