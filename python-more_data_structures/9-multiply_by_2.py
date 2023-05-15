68d05ec2a885d6a66e1c8b130ff72bbf788de8bb
Rob <115587964+Nomad-Rob@users.noreply.github.com>
2023-05-15 13:41:25 -0500

    pycodestyle corrections

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Write a function that returns a new dictionary with all values multiplied by 2
+# Write a function that returns anew dictionary withall values multiplied by 2
 def multiply_by_2(a_dictionary):
     new_dict = {}
     for key, value in a_dictionary.items():
