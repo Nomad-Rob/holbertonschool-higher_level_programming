090fcc4a6e0daaa89bc571e18afa6af3b7c49780
Rob <115587964+Nomad-Rob@users.noreply.github.com>
2023-05-19 14:57:08 -0500

    trying again

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Write a class Square that defines a square by: (based on 5-square.py)
-"""Define a Square class."""
+"""Define a Square class"""
 
 class Square:
     """Square Class"""
