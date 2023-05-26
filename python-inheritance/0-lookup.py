0097a15ab85ced90375f35b5dfc6d5f31debf6b0
Rob <115587964+Nomad-Rob@users.noreply.github.com>
2023-05-26 08:54:28 -0500

    adding task 0 to make sure pep8 and checker works with the code

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Write a function that returns the list of 
+# available attributes and methods of an object
+"""This defines the lookup module, which is returning the list 
+of available attributes and methods of an object"""
+
+
+def lookup(obj):
+    """Returns the list of available attributes and methods of an object"""
+    return dir(obj)
