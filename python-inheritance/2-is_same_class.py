2f6747742a12ee3e29478eb5f9ac83619326def5
Nomad-Rob <rfarley89@gmail.com>
2023-05-26 16:16:42 -0500

    checking if my Class will work

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Wrtie a function that returns True if the object is 
+# exactly an instance of the specified class ; otherwise False
+"""The is_same_class function returns True if the 
+object is exactly an instance"""
+
+
+def is_same_class(obj, a_class):
+    """is_same_class function returns True if the object
+    is exactly an instance of the specified class such as int, float, etc"""
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
