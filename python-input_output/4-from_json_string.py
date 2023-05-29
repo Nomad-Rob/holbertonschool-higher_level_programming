764e94d4bffcf3ba5d41d224581c841da7a26813
Nomad-Rob <rfarley89@gmail.com>
2023-05-29 11:52:20 -0500

    another pep8 change

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-""" A function that returns an object represented by a JSON string
+"""Write a function that returns an object
+(Python data structure) represented by a JSON string:
 Prototype: def from_json_string(my_str):
-You don’t need to manage exceptions if the string doesn’t
-represent an object."""
+You don’t need to manage exceptions if the JSON
+string doesn’t represent an object.
+"""
 
 
 def from_json_string(my_str):
