79c722eaeea907960b7c606d954ba3c4afcf2fb5
Nomad-Rob <rfarley89@gmail.com>
2023-05-30 13:14:18 -0500

    another change to code to fix errors

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,11 +7,18 @@ The list must be saved as a JSON representation in a file named add_item.json
 You don’t need to manage arguments passed to the script (see example below)"""
 
 
-import sys
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+save_json = __import__("5-save_to_json_file").save_to_json_file
+load_json = __import__("6-load_from_json_file").load_from_json_file
+
+filename = "add_item.json"
 
 try:
-    my_list = load_from_json_file("add_item.json")
-except:
-    my_list = []
+    json_list = load_json(filename)
+
+except(TypeError, FileNotFoundError):
+    json_list = []
+
+for i in sys.argv[1:]:
+    json_list.append(i)
+
+save_json(json_list, filename)
