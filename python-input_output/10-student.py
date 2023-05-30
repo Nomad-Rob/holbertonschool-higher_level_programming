17d69bf9e2cd517e29ddb18a62d7ad53a3fd96af
Nomad-Rob <rfarley89@gmail.com>
2023-05-30 13:26:39 -0500

    Not sure on exception or for statement just yet

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -14,6 +14,24 @@ Otherwise, all attributes must be retrieved
 You are not allowed to import any module"""
 
 
-def class_to_json(obj):
-    """returns the dictionary description with simple data structure"""
-    return obj.__dict__
+class Student:
+    """
+    Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ dictionary representation of a Student instance
+         """
+        if attrs is None:
+            return vars(self)
+        new_dict = {}
+        for i in attrs:
+            try:
+                new_dict[i] = vars(self)[i]
+            except Exception:
+                pass
+        return new_dict
