695a9450f4354adceb683803a73032c500b2e221
Nomad-Rob <rfarley89@gmail.com>
2023-05-30 13:06:09 -0500

    reworked try and except code

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,13 +8,15 @@ You don’t need to manage arguments passed to the script (see example below)"""
 
 
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 try:
-    my_list = load_from_json_file("add_item.json")
+    file = load_from_json_file("add_item.json")
 except:
-    my_list = []
-for i in range(1, len(sys.argv)):
-    my_list.append(sys.argv[i])
-save_to_json_file(my_list, "add_item.json")
+    file = []
+    
+argc = len(sys.argv)
+for i in range(1, argc):
+    file.append(sys.argv[i])
+save_to_json_file(file, "add_item.json")
