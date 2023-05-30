2673cd2f8542a8ace8eab736acfcbb7c68a86186
Nomad-Rob <rfarley89@gmail.com>
2023-05-30 13:49:38 -0500

    will try again later

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -12,11 +12,4 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-if __name__ == "__main__":
-    try:
-        my_list = load_from_json_file("add_item.json")
-    except:
-        my_list = []
-    for i in range(1, len(sys.argv)):
-        my_list.append(sys.argv[i])
-    save_to_json_file(my_list, "add_item.json")
+# Will try more later.. cant get it working
