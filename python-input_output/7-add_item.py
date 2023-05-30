b08ab382b4c0979c3a87ab3c30dbad9e2ac25fd0
Nomad-Rob <rfarley89@gmail.com>
2023-05-30 13:40:18 -0500

    anotehr try

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,4 +7,16 @@ The list must be saved as a JSON representation in a file named add_item.json
 You don’t need to manage arguments passed to the script (see example below)"""
 
 
-#need to rewrite all code for this one.. 
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+def load_add_save():
+    """adds all arguments to a Python list, and then save them to a file"""
+    try:
+        my_list = load_from_json_file("add_item.json")
+    except:
+        my_list = []
+    for i in range(1, len(sys.argv)):
+        my_list.append(sys.argv[i])
+    save_to_json_file(my_list, "add_item.json")
