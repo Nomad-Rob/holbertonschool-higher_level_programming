8c05c502d7badba8281550fa5a645aec83724688
Nomad-Rob <rfarley89@gmail.com>
2023-05-30 12:32:28 -0500

    using mode r

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" A function that creates an Object from a “JSON file”:
+Prototype: def load_from_json_file(filename):
+You must use the with statement
+You don’t need to manage exceptions if the JSON 
+string doesn’t represent an object.
+You don’t need to manage file permissions / exceptions."""
+
+
+def load_from_json_file(filename):
+    """function that creates an Object from a “JSON file”"""
+    with open(filename, mode='r', encoding='utf-8') as f:
+        import json
+        return json.load(f)
