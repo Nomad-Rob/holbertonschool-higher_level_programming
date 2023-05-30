abee8205cd3708a40f0cec9d429413370962497c
Nomad-Rob <rfarley89@gmail.com>
2023-05-30 13:47:52 -0500

    last try

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -13,10 +13,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if __name__ == "__main__":
-    """rocking the main"""
     try:
         my_list = load_from_json_file("add_item.json")
-    except FileNotFoundError:
+    except:
         my_list = []
-    my_list.extend(sys.argv[1:])
+    for i in range(1, len(sys.argv)):
+        my_list.append(sys.argv[i])
     save_to_json_file(my_list, "add_item.json")
