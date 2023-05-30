2f20c960beb0659f484a33b7a6f241ac4f23f258
Nomad-Rob <rfarley89@gmail.com>
2023-05-30 13:46:44 -0500

    antoher another another try

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,16 +8,15 @@ You don’t need to manage arguments passed to the script (see example below)"""
 
 
 import sys
-import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
 
-if os.path.exists(filename):
-    my_list = load_from_json_file(filename)
-else:
-    my_list = []
-for i in range(1, len(sys.argv)):
-    my_list.append(sys.argv[i])
-save_to_json_file(my_list, filename)
+if __name__ == "__main__":
+    """rocking the main"""
+    try:
+        my_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        my_list = []
+    my_list.extend(sys.argv[1:])
+    save_to_json_file(my_list, "add_item.json")
