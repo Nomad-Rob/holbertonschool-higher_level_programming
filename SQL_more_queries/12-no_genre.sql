f8348af47340d8e48e3984c292fcfd3bf9907fa4
Nomad-Rob <rfarley89@gmail.com>
2023-06-27 13:11:37 -0500

    adding all of the project. all in workign order no errors

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,2 @@
+--lists all shows that have no genre data
+SELECT tv_shows.title, tv_show_genres.genre_id FROM (tv_show_genres RIGHT OUTER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id) WHERE tv_show_genres.genre_id IS NULL ORDER BY tv_shows.title ASC;
