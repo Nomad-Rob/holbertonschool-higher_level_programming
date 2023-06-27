a1b6145659828341f185dfd606371ed193dce311
Nomad-Rob <rfarley89@gmail.com>
2023-06-27 14:47:31 -0500

    syntax change

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,8 +1,11 @@
 --creates a database named hbtn_0d_usa containing a table named cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    FOREIGN KEY(state_id) REFERENCES states(id));
+CREATE TABLE IF NOT EXISTS cities (
+  id INT NOT NULL AUTO_INCREMENT,
+  state_id INT NOT NULL,
+  name VARCHAR(256) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (id),
+  FOREIGN KEY (state_id) REFERENCES states(id)
+);
