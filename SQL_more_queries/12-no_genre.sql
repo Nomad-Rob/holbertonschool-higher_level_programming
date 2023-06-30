49ee194e7f2fb84da7536d446087588b2a660fab
Nomad-Rob <rfarley89@gmail.com>
2023-06-29 20:04:55 -0500

    trying these two task out again vs checker

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,2 +1,6 @@
---lists all shows that have no genre data
-SELECT tv_shows.title, tv_show_genres.genre_id FROM (tv_show_genres RIGHT OUTER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id) WHERE tv_show_genres.genre_id IS NULL ORDER BY tv_shows.title ASC;
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 11-genre_id_all_shows.sql)
+SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_genres.id is NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
