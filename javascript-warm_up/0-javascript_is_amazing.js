d1221205657b0d56b198ff3c6f54084db06a8fd3
Nomad-Rob <rfarley89@gmail.com>
2023-07-27 11:25:59 -0500

    adding task 1 working

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// Task 0, Hello world
+// Task 0 - Write a script that prints “JavaScript is amazing”:
 
 const myVar = 'JavaScript is amazing';
 
---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Task 1 - Write a script that prints 3 lines:
+
+const myarray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const myVar of myarray) {
+  console.log(myVar);
+}
