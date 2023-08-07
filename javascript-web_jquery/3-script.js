2f0329c274b65e393b1e43a6f3b7e8e869bd8c5d
Nomad-Rob <rfarley89@gmail.com>
2023-08-07 18:01:07 -0500

    adding working files for this project, task 9 is not working in browerser with 2 different code sets. will try more tomoorow

---
+++ javascript-web_jquery/3-script.js
@@ -0,0 +1,7 @@
+// Update class of <header> tag to red, using JQUERY
+
+$('div#red_header').click(
+  function () {
+    $('header').addClass('red');
+  }
+);
---
+++ javascript-web_jquery/9-script.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// fetch Hello, from the headtag
+
+const url = 'https://stefanbohacek.com/hellosalut/?lang=fr';
+
+window.onload = function () {
+  $.getJSON(url, function (data, status) {
+    $('DIV#hello').text(data.hello);
+  });
+};
+
+// also not working below 
+// $(document).ready(function() {
+    // Fetch the translation of "hello" from the provided URL
+    //$.get('https://stefanbohacek.com/hellosalut/?lang=fr', function(data) {
+      // Extract the value of "hello" from the response
+     // const helloValue = $(data).find('DIV#hello').text();
+      
+      // Set the extracted value as the content of DIV#hello
+     //$('#hello').text(helloValue);
+  //  });
+  // });
